05bcdf9c32571efc66977cb9769bb3b45df7edbc
David Arinder <34454124+davidArinder@users.noreply.github.com>
2019-04-09 19:21:56 -0700

    update index.js

--- js/index.js
+++ js/index.js
@@ -103,8 +103,8 @@ const updateCalculatorState = (key, calculator, calculatedValue, displayedNum) =
     const keyType = getKeyType(key);
     Array.from(key.parentNode.children).forEach(k => k.classList.remove("is-depressed"))
 
-    if(keyType === "operator") key.classList.add("is-depressed")
-    if(keyType === "clear" && key.textContent !== "AC") key.textContent = "AC"
+    if (keyType === "operator") key.classList.add("is-depressed")
+    if (keyType === "clear" && key.textContent !== "AC") key.textContent = "AC"
     if (keyType !== "clear") {
       const clearButton = calculator.querySelector("[data-action=clear]")
       clearButton.textContent = "CE"
